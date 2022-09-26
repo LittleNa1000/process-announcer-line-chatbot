731fe79f9813767200051706a41cb8bde506faa0
LittleNa1000 <naphanccnp@gmail.com>
2022-09-26 23:16:52 +0700

    ignore process files

--- src/readcsv.js
+++ src/readcsv.js
@@ -3,7 +3,7 @@ const { parse } = require("csv-parse");
 const { PROCESS_FILE_NAME } = require("./constants");
 
 const csvPromise = new Promise((resolve, reject) => {
-  fs.readFile(`./resources/${PROCESS_FILE_NAME}`, (err, fileData) => {
+  fs.readFile(`./process-files/${PROCESS_FILE_NAME}`, (err, fileData) => {
     parse(fileData, {}, function (err, rows) {
       resolve(rows);
     });
